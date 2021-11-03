d56e0923697adf559774caec0c4f7e3fee57a4ed
dave nunez <noonz2@icloud.com>
2021-11-03 02:25:42 +0000

    implement react, auth states, actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import itemReducer from './ItemReducer';
+import authReducer from './authReducer';
+import errorReducer from './errorReducer';
 
 export default combineReducers({
-    item: itemReducer
+    item: itemReducer,
+    error: errorReducer,
+    auth: authReducer
 });
